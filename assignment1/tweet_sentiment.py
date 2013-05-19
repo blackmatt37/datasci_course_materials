2b24f1dca37466107dc9cc8a01c8e836f821fb65
Matthew Kramer <matthewkram@gmail.com>
2013-05-19 21:46:04 +0000

    finished tweet_sentiemnt

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,5 +1,5 @@
 import sys
-
+import json
 def hw():
     print 'Hello, world!'
 
@@ -7,10 +7,22 @@ def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
+    afinnfile = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
+    scores = {}
+    for line in afinnfile:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    for line in tweet_file:
+        tweet = json.loads(line)
+        text = tweet["text"]
+        words = text.split[" "]
+        score = 0
+        for word in words:
+            score += scores[word]
+        print float(score)
     hw()
-    lines(sent_file)
+    lines(afinnfile)
     lines(tweet_file)
 
 if __name__ == '__main__':
